01ae5ba4813f2fa88278f482683734a5e28419c5
Abhinav's MacBook Air M3 <psiecsaalry@gmail.com>
2025-03-04 13:11:49 +0530

    [Abhinav] ADD: FactorsArray.java

---
+++ FactorsArray.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class FactorsArray {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // Get user input
+        System.out.print("Enter a number: ");
+        int number = input.nextInt();
+
+        // Initial size of factors array
+        int maxFactor = 10;
+        int[] factors = new int[maxFactor];
+        int index = 0;
+
+        // Find factors
+        for (int i = 1; i <= number; i++) {
+            if (number % i == 0) {
+                // Resize array if needed
+                if (index == maxFactor) {
+                    maxFactor *= 2;
+                    int[] temp = new int[maxFactor];
+                    System.arraycopy(factors, 0, temp, 0, factors.length);
+                    factors = temp;
+                }
+                factors[index++] = i;
+            }
+        }
+
+        // Print factors
+        System.out.print("Factors of " + number + " are: ");
+        for (int i = 0; i < index; i++) {
+            System.out.print(factors[i] + " ");
+        }
+
+        input.close();
+    }
+}
